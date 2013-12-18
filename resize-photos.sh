b91da82e0f4a98901e172e21ca5b80bc7ffd9ddc
Ondrej Kulaty <kulaty.o@gmail.com>
2013-12-18 15:24:26 +0100

    Added a few more options to resize-photos.sh

--- resize-photos.sh
+++ resize-photos.sh
@@ -8,63 +8,71 @@
 # It will also create album thumbnail
 
 PROGRAM_NAME="resize-photos"
-
-# Functions
-echo_message() {
-    echo "[${PROGRAM_NAME}]: $@"
-}
-
-# Same as message but prints to stderr
-echo_err() {
-echo_message "$@" 1>&2
-}
-
-# Create dir and check if succeded. If not, terminate.
-mkdir_and_check() {
-    mkdir "$1"
-    if [ $? -ne 0 ]; then
-        echo_err "Creating directory failed!"
-        exit 1
-    fi
-}
-# exit script (launched when user hits Control-C)
-control_c() {
-    echo -en "\n*** Ouch! Exiting ***\n"
-    #cleanup
-    exit $?
-}
+source functions.sh
 
 # trap interrupt signal
 trap control_c SIGINT 
 
 # 1) read album name
 ALBUM_NAME=""
-echo "Name of the album: "
+echo_highlight "Name of the album: "
 read -e ALBUM_NAME
 if [ "$ALBUM_NAME" == "" ]; then
     echo_err "Album name cannot be empty!"
     exit 1
 fi
 
-# 2) create output directory
-OUTPUT_DIR="./output"
-mkdir_and_check ${OUTPUT_DIR}
-mkdir_and_check ${OUTPUT_DIR}/${ALBUM_NAME}
+# 2) read input directory
+INPUT_DIR=""
+echo_highlight "Input directory [./]: "
+read -e INPUT_DIR
+if [ "$INPUT_DIR" == "" ]; then
+    INPUT_DIR="./"
+fi
+if [ ! -d "$INPUT_DIR" ]; then
+    echo_err "Input directory (${INPUT_DIR})  does not exist!"
+    exit 1
+fi
 
-# 3) resize photos
+# 4) create output directory
+OUTPUT_DIR=""
+echo_highlight "Output directory [./output/]: "
+read -e OUTPUT_DIR
+if [ "$OUTPUT_DIR" == "" ]; then
+    OUTPUT_DIR="./output/"
+fi
+mkdir_and_check "${OUTPUT_DIR}${ALBUM_NAME}"
 
+# 5) photo size and annotation 
+PHOTO_SIZE_OPTION=""
+PHOTO_SIZE=""
+echo_highlight "Choose preset:"
+echo "1) Caption: Hanačka Litovel, Size: 450000 (approx. 822x548)"
+echo "2) Caption: Hanácká mozeka Litovel, size: 785000 (approx. 1024x768)"
 
-COUNTER=0
-for i in *.{jpg,JPG}; do
+read -e PHOTO_SIZE_OPTION 
+case $PHOTO_SIZE_OPTION in
+    1) PHOTO_SIZE=450000; CAPTION="Hanačka Litovel" ;;
+    2) PHOTO_SIZE=785000; CAPTION="Hanácká mozeka Litovel" ;;
+    *) echo_err "Invalid option"; exit 1 ;;
+esac
+
+# 6) resize photos
+for i in "${INPUT_DIR}"*.{jpg,JPG}; do
     if [ -f "${i}" ]; then
-        echo "Resizing ${i}"
-        convert "${i}" -quality 85 -auto-orient -strip -resize "@450000" ${OUTPUT_DIR}/${ALBUM_NAME}/image-${COUNTER}.jpg 
-        COUNTER=$(($COUNTER+1))
+        echo_message "Resizing ${i}"
+        convert "${i}" -quality 85 -auto-orient -strip -resize "@${PHOTO_SIZE}" \
+        -gravity SouthEast \
+        -stroke '#000C' -strokewidth 2 -annotate +5+5 "${CAPTION}" \
+        -stroke  none   -fill white    -annotate +5+5 "${CAPTION}" \
+        "${OUTPUT_DIR}${ALBUM_NAME}"/image-`date +%s%N`.jpg 
     fi
 done
 
-# 4) album thumbnail
-echo "Specify photo filename which will be used as album thumbnail:"
+# 7) album thumbnail
+echo_highlight "Specify photo filename which will be used as album thumbnail (leave empty for no thumbnail):"
 read -e ALBUM_THUMBNAIL
-convert ${ALBUM_THUMBNAIL} -resize x120 -gravity center  -crop 160x120+0+0 -quality 85 -strip +repage ${OUTPUT_DIR}/${ALBUM_NAME}.jpg
-echo "Done!"
+if [ -f "${ALBUM_THUMBNAIL}" ]; then
+    convert "${ALBUM_THUMBNAIL}" -resize x120 -gravity center  -crop 160x120+0+0 -quality 85 -strip +repage "${OUTPUT_DIR}${ALBUM_NAME}".jpg
+fi
+echo_message "Done!"
